1e585cf0a515e9cc1aebd65cd645eb7514507fe8
Nguyen <mr.nguyencongtuan@gmail.com>
2020-03-09 17:11:41 +0700

    Setup PHP Unit Test for Travis CI

---
+++ tests/test-sample.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Sample_Plugin
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_first() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+
+	function test_second() {
+		$this->assertEquals( 1, 0 );
+	}
+}
