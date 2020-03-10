c73c7a7820b53eb708be93cff47c95df3885c0ef
Nguyen <mr.nguyencongtuan@gmail.com>
2020-03-10 11:22:48 +0700

    Add some matrix for check with nightly version of WP

--- tests/unit-tests/test-shortcode.php
+++ tests/unit-tests/test-shortcode.php
@@ -28,4 +28,14 @@ class a3Rev_Tests_PVC_Shortcode extends WP_UnitTestCase {
 
 		$this->assertStringContainsString( 'views today' , $output );
 	}
+
+	function test_hide_views_today() {
+		$pvc_settings = get_option( 'pvc_settings', array() );
+		$pvc_settings['views_type'] = 'total_only';
+		update_option( 'pvc_settings', $pvc_settings );
+
+		$output = do_shortcode( '[pvc_stats postid=1 show_views_today=0]' );
+
+		$this->assertStringNotContainsString( 'views today' , $output );
+	}
 }
