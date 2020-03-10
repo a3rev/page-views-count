55122e3e1ffbc0b41b418197eb00aab1c3aa32eb
Nguyen <mr.nguyencongtuan@gmail.com>
2020-03-10 13:09:59 +0700

    Update eye icon

--- includes/customized_style.php
+++ includes/customized_style.php
@@ -6,8 +6,9 @@ global $pvc_settings;
 	clear: both;
 }
 /* Stats Icon */
-body .pvc-stats-icon {
+body .pvc-stats-icon, body .pvc-stats-icon svg {
 	color: <?php echo $pvc_settings['icon_color']; ?> !important;
+	fill: <?php echo $pvc_settings['icon_color']; ?> !important;
 }
 body .pvc_stats {
 <?php if ( 'centre' == $pvc_settings['aligment'] ) { ?>
@@ -22,12 +23,12 @@ body .pvc_stats .pvc-stats-icon {
 	vertical-align: middle;
 }
 body .pvc_stats .pvc-stats-icon.small svg {
-	width: 16px;
+	width: 18px;
 }
 body .pvc_stats .pvc-stats-icon.medium svg {
-	width: 20px;
+	width: 24px;
 }
 body .pvc_stats .pvc-stats-icon.large svg {
-	width: 24px;
+	width: 30px;
 }
 </style>
